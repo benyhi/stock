bbafd6986e3fe677f8b2ecd215bd7b71901470cc
Benja <sosabenjaminmartin09@gmail.com>
2024-05-07 00:00:07 -0300

    Retoque algunas cosas del front y hice la funcionalidad para eliminar items de la tabla

--- manejo.py
+++ manejo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -19,6 +19,17 @@ def index():
     
     # Pasar los datos a la plantilla HTML y renderizarla
     return render_template('stock.html', data=data)
+
+@app.route('/eliminar/<string:id>')
+def eliminar(id):
+    conn = sqlite3.connect('stock.db')
+    c = conn.cursor()
+    c.execute(f'DELETE FROM articulos WHERE id = {id}')
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
